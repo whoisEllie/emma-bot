99ab04b60a8bdb8115840666022886d9106d2d45
whoisEllie <elliekelemen@protonmail.com>
2023-12-26 01:30:04 +0100

    Add ping for help role

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const fs = require('node:fs');
-const path = require('node:path')
-const { Client, Partials, GatewayIntentBits, Collection, Events } = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  Partials,
+  GatewayIntentBits,
+  Collection,
+  Events,
+} = require("discord.js");
 const { ReactionRole } = require("discordjs-reaction-role");
 
 // the config file contains two fields:
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
 // the client is created with the partial message option to capture events for uncached messages
 // if this options is not set, the bot may not be aware of the message that it should be watching
@@ -19,12 +25,12 @@ const client = new Client({
 
 client.commands = new Collection();
 
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   const command = interaction.client.commands.get(interaction.commandName);
 
-  if(!command) {
+  if (!command) {
     console.error(`No command matching ${interaction.commandName} was found.`);
     return;
   }
@@ -33,30 +39,46 @@ client.on(Events.InteractionCreate, async interaction => {
     await command.execute(interaction);
   } catch (error) {
     console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true})
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   }
-})
+});
 
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
 
-  if ('data' in command && 'execute' in command) {
+  if ("data" in command && "execute" in command) {
     client.commands.set(command.data.name, command);
   } else {
-    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`);
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`,
+    );
   }
 }
 
 const ReactionRoleClass = new ReactionRole(client, [
-  {messageId: "1038879978307203174", roleId: "1038879877098651678", reaction: "‼️"}
-])
+  {
+    messageId: "1038879978307203174",
+    roleId: "1038879877098651678",
+    reaction: "‼️",
+  },
+  {
+    messageId: "1038879978307203174",
+    roleId: "1189001687629832193",
+    reaction: "❓",
+  },
+]);
 
 client.login(config.token);
 
 client.on("ready", () => {
-  console.log(`Watching message for reactions...`)
+  console.log(`Watching message for reactions...`);
 });
